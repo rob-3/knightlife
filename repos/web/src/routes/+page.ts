21ee089d1be0887c6e2a60382c3cdd86a5ecbbcb
Raciel <git@raciel.dev>
2023-10-05 20:21:56 -0400

    refactor(web): lint

--- repos/web/src/routes/+page.ts
+++ repos/web/src/routes/+page.ts
@@ -1,8 +1,8 @@
-import type { PageLoad } from './$types';
-import type { SampleType } from 'types';
+import type { PageLoad } from "./$types";
+import type { SampleType } from "types";
 
 export const load: PageLoad = async () => {
-  const res = await fetch('http://localhost:5000/sampleData');
+  const res = await fetch("http://localhost:5000/sampleData");
   const { data } = (await res.json()) as { data: SampleType[] };
   console.log(data);
   return { data };

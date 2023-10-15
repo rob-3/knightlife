eaedff99bf22a8f3dc1af5e83aa55fbdad03df17
Robert Boyd III <git@robertboyd.dev>
2023-10-15 16:57:04 -0400

    yellow

--- repos/web/src/routes/+page.svelte
+++ repos/web/src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 <button
   on:click={() => (count += 1)}
-  class="rounded-2xl bg-pink-500 px-8 py-4 text-4xl font-bold text-white hover:bg-purple-700"
+  class="rounded-2xl bg-yellow-500 px-8 py-4 text-4xl font-bold text-white hover:bg-purple-700"
 >
   Clicks: {count}
 </button>
